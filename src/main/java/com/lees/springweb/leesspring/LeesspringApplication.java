1206e7503a921326a5d5e3108021347aa9fbf250
DevLeeYG <hi33741@naver.com>
2022-04-01 15:29:16 +0900

    Initial commit

---
+++ src/main/java/com/lees/springweb/leesspring/LeesspringApplication.java
@@ -0,0 +1,13 @@
+package com.lees.springweb.leesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeesspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeesspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lees/springweb/leesspring/LeesspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lees.springweb.leesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeesspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
